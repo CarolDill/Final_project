98c9ceff02524beee9df5ab3dbac894d9d9ff7c8
CarolDill <carol37_dill@msn.com>
2022-06-07 22:14:42 -0700

    frontpage mobile complete
    redundancy removed on css (commented to be deleted later).
    Menu navbar finished (javascript) with sticky position and color change.
    missing: responsiveness to media queries.

--- scripts.js
+++ scripts.js
@@ -16,3 +16,24 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("stickyheader");
+// var header = document.getElementsByClassName("headerwrapper");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky")
+      navbar.classList.add("dark")
+    } else {
+      navbar.classList.remove("sticky");
+      navbar.classList.remove("dark");
+    }
+  }
